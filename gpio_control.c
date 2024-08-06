a59fa121cce10eb2cb49db97d0c89c02e1261e4c
sampark8 <73883388+sampark8@users.noreply.github.com>
2024-08-06 16:26:49 +0900

    yes

--- gpio_control.c
+++ gpio_control.c
@@ -1,185 +1,37 @@
 #include "gpio_control.h"
 
-
+BLE_NUS_DEF(m_nus, NRF_SDH_BLE_TOTAL_LINK_COUNT);   
+static uint16_t   m_conn_handle          = BLE_CONN_HANDLE_INVALID;       
 static uint32_t i;
+int pulse_status = 0;
+//int idle_loop = 124;
+int idle_loop = 248;
+int looper = 0;
+int test_looper = 0;
+//int* pulse_arr;
 
+static int16_t buff = 1;
+int pulse_no;
+bool pulseSending = false;
+bool pulse_complete = false;
+bool pulseTesting = false;
+bool testFinished = false;
 void gpio_init(void)
 {   
-
     // Initialize Control Pins
-    nrf_gpio_cfg_output(CONTROL_PIN_1);
-    nrf_gpio_cfg_output(CONTROL_PIN_2);
-    nrf_gpio_cfg_output(CONTROL_PIN_3);
-    nrf_gpio_cfg_output(CONTROL_PIN_4);
-
-    // Initialize LED Pins
-    nrf_gpio_cfg_output(LED_PIN_1);
-    nrf_gpio_cfg_output(LED_PIN_2); 
-    nrf_gpio_cfg_output(LED_PIN_3); 
-    nrf_gpio_cfg_output(LED_PIN_4); 
-    nrf_gpio_cfg_output(LED_PIN_5); 
-
+    nrf_gpio_cfg_output(PULSE_PIN_1);
+    nrf_gpio_cfg_output(PULSE_PIN_2);
+    nrf_gpio_cfg_output(TEST_PIN);
     // Turn off Control Pins
-    nrf_gpio_pin_clear(CONTROL_PIN_1);
-    nrf_gpio_pin_clear(CONTROL_PIN_2);
-    nrf_gpio_pin_clear(CONTROL_PIN_3);
-    nrf_gpio_pin_clear(CONTROL_PIN_4);
-
-    // Turn off LEDs
-    nrf_gpio_pin_clear(LED_PIN_1);
-    nrf_gpio_pin_clear(LED_PIN_2); 
-    nrf_gpio_pin_clear(LED_PIN_3); 
-    nrf_gpio_pin_clear(LED_PIN_4); 
-    nrf_gpio_pin_clear(LED_PIN_5); 
-
-    uint32_t err_code = app_timer_start(m_app_timer_id, LED_INTERVAL, NULL); // initialize the timer
+    nrf_gpio_pin_clear(PULSE_PIN_1);
+    nrf_gpio_pin_clear(PULSE_PIN_2);
+    nrf_gpio_pin_clear(TEST_PIN);
+    app_timer_start(m_app_timer_id, PULSE_INTERVAL, NULL); // initialize the timer
+    app_timer_start(pulse_test_id, TEST_INTERVAL, NULL); // initialize the timer
 }
 
 /* Turn on and off Control Pins */
-void control_pin1_onoff(int onoff)
-{
-    if(onoff == 1) nrf_gpio_pin_set(CONTROL_PIN_1);
-    else nrf_gpio_pin_clear(CONTROL_PIN_1);
-}
-
-void control_pin2_onoff(int onoff)
-{
-    if(onoff == 1) nrf_gpio_pin_set(CONTROL_PIN_2);
-    else nrf_gpio_pin_clear(CONTROL_PIN_2);
-}
-
-void control_pin3_onoff(int onoff)
-{
-    if(onoff == 1) nrf_gpio_pin_set(CONTROL_PIN_3);
-    else nrf_gpio_pin_clear(CONTROL_PIN_3);
-}
-
-void control_pin4_onoff(int onoff)
-{
-    if(onoff == 1) nrf_gpio_pin_set(CONTROL_PIN_4);
-    else nrf_gpio_pin_clear(CONTROL_PIN_4);
-}
-
-void control_table(ble_nus_evt_t * p_evt)
-{
-    int idx = 0;
-    char buffer_rx[20] = "";
-    if (p_evt->type == BLE_NUS_EVT_RX_DATA)
-    {
-        uint32_t err_code;
-        NRF_LOG_DEBUG("Received data from BLE NUS. Writing data on UART.");
-        NRF_LOG_HEXDUMP_DEBUG(p_evt->params.rx_data.p_data, p_evt->params.rx_data.length);
-        for (uint32_t i = 0; i < p_evt->params.rx_data.length; i++)
-        {
-            do
-            {
-                err_code = app_uart_put(p_evt->params.rx_data.p_data[i]);
-                
-                if ((err_code != NRF_SUCCESS) && (err_code != NRF_ERROR_BUSY))
-                {
-                    NRF_LOG_ERROR("Failed receiving NUS message. Error 0x%x. ", err_code);
-                    APP_ERROR_CHECK(err_code);
-                }
-
-            } while (err_code == NRF_ERROR_BUSY);
-            if(p_evt->params.rx_data.p_data[i] == '<' || p_evt->params.rx_data.p_data[i] == '>') continue;
-            buffer_rx[idx] = p_evt->params.rx_data.p_data[i];
-            idx++;
-        }
-        if (p_evt->params.rx_data.p_data[p_evt->params.rx_data.length - 1] == '\r')
-        {
-            while (app_uart_put('\n') == NRF_ERROR_BUSY);
-        }
-        
-        printf("\r\nCENTRAL RECEIVED MESSAGE:::%s\r\n", buffer_rx);
-
-        // operate LED
-        if(strcmp(buffer_rx, "LEDON") == 0)             led_on(); 
-        else if(strcmp(buffer_rx, "LEDOFF") == 0)       led_off(); 
-        else if(strcmp(buffer_rx, "LEDCASCADEON")==0)   led_cascade_on();
-        else if(strcmp(buffer_rx, "LEDCASCADEOFF")==0)  led_cascade_off();
-        else if(strcmp(buffer_rx, "CONTROLPIN1ON")==0)  control_pin1_onoff(1);
-        else if(strcmp(buffer_rx, "CONTROLPIN1OFF")==0) control_pin1_onoff(0); 
-        else if(strcmp(buffer_rx, "CONTROLPIN2ON")==0)  control_pin2_onoff(1);
-        else if(strcmp(buffer_rx, "CONTROLPIN2OFF")==0) control_pin2_onoff(0);
-        else if(strcmp(buffer_rx, "CONTROLPIN3ON")==0)  control_pin3_onoff(1);
-        else if(strcmp(buffer_rx, "CONTROLPIN3OFF")==0) control_pin3_onoff(0);
-        else if(strcmp(buffer_rx, "CONTROLPIN4ON")==0)  control_pin4_onoff(1);
-        else if(strcmp(buffer_rx, "CONTROLPIN4OFF")==0) control_pin4_onoff(0);
-    }
-}
-
-static void app_timer_handler(void * p_context)
-{
-      static bool status1, status2, status3, status4, status5;
-
-      status1 = nrf_gpio_pin_out_read(LED_PIN_1);
-      status2 = nrf_gpio_pin_out_read(LED_PIN_2);
-      status3 = nrf_gpio_pin_out_read(LED_PIN_3);
-      status4 = nrf_gpio_pin_out_read(LED_PIN_4);
-      status5 = nrf_gpio_pin_out_read(LED_PIN_5);
-
-      if (status1 && status2 && status3 && status4 && status5)
-      {
-          // Turn off all LEDs
-          nrf_gpio_pin_clear(LED_PIN_1);
-          nrf_gpio_pin_clear(LED_PIN_2); 
-          nrf_gpio_pin_clear(LED_PIN_3); 
-          nrf_gpio_pin_clear(LED_PIN_4); 
-          nrf_gpio_pin_clear(LED_PIN_5); 
-      }
-      else if(!status1) nrf_gpio_pin_set(LED_PIN_1);
-      else if(!status2) nrf_gpio_pin_set(LED_PIN_2);
-      else if(!status3) nrf_gpio_pin_set(LED_PIN_3);
-      else if(!status4) nrf_gpio_pin_set(LED_PIN_4);
-      else if(!status5) nrf_gpio_pin_set(LED_PIN_5);
-}
-
-void create_timer(void)
-{
-    ret_code_t err_code;
-    err_code = app_timer_create(&m_app_timer_id, APP_TIMER_MODE_REPEATED, app_timer_handler);
-    APP_ERROR_CHECK(err_code);    
-}
-
-/* LED Control Functions */
-void led_on(void)
-{
-    printf("FUNC LED ON\r\n");
-    nrf_gpio_pin_set(LED_PIN_1);
-}
-
-void led_off(void)
-{
-    printf("FUNC LED OFF\r\n");
-    nrf_gpio_pin_clear(LED_PIN_1);
-}
-
-void led_cascade_on(void)
-{
-    printf("FUNC LED CASCADE ON\r\n");
-    if (app_timer_cnt_get() == 0)
-    {
-      create_timer();
-      app_timer_start(m_app_timer_id, LED_INTERVAL, NULL);  // initialize the timer
-    }
-}
 
 
-/* Turn off all LEDs*/
-void led_cascade_off(void)
-{
-    printf("FUNC LED CASCADE OFF\r\n");
-    if (app_timer_cnt_get() != 0)
-    {
-        app_timer_stop(m_app_timer_id);
-        nrf_gpio_pin_clear(LED_PIN_1);
-        nrf_gpio_pin_clear(LED_PIN_2); 
-        nrf_gpio_pin_clear(LED_PIN_3);
-        nrf_gpio_pin_clear(LED_PIN_4);
-        nrf_gpio_pin_clear(LED_PIN_5); 
-        i = 0;
-    }
-}
 
 
--- main.c
+++ main.c
@@ -2,45 +2,55 @@
 
 
 /**@brief Application main function.
+
  */
+
+
 int main(void)
 {
     ret_code_t err_code;
-
+    
     bool erase_bonds;
-
+    bool saadc_enabled = false;
+    //err_code = ble_dfu_buttonless_async_svci_init();
     // Initialize.
     gpio_init();
     uart_init();
     log_init();
     lfclk_config();
     timers_init();
-    buttons_leds_init(&erase_bonds);
+    //buttons_leds_init(&erase_bonds);
     ble_stack_init();
     gap_params_init();
     gatt_init();
     services_init();
+    twi_master_init();
+    nrf_delay_ms(1000);
+    
     advertising_init();
-
-    // Advertising Strength selected 
-    err_code = sd_ble_gap_tx_power_set(BLE_GAP_TX_POWER_ROLE_ADV, m_advertising.adv_handle, +4);
+    
+    err_code = sd_ble_gap_tx_power_set(BLE_GAP_TX_POWER_ROLE_ADV, m_advertising.adv_handle, 0);
     APP_ERROR_CHECK(err_code);
 
     conn_params_init();
-
-    saadc_sampling_event_init();
-    saadc_init();
-    saadc_sampling_event_enable();
-
-    // Start execution.
-    printf("\r\nUART started.\r\n");
+    
+    
+    while(mpu6050_init() == false)
+    {
+      printf("MPU_6050 initialization failed!!!"); 
+      nrf_delay_ms(1000);
+    }
+    printf("MPU6050 Init Success");
+    // Start execution.C:\NRF_SDK17_MAIN\nRF5_SDK_17.1.0_ddde560\examples\JOOJANGHOON_PROJ\TWI_TEST_2\main.c
     NRF_LOG_INFO("Debug logging for UART over RTT started.");
+    sw_timer_init();
     advertising_start();
 
-    // Enter main loop.
-    for (;;)
-    {
+    for(;;){
+      //sd_app_evt_wait();
     }
+
+
 }
 
 
